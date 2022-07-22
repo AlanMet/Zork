7fb07baacac3044cde4ea15b6489f9d2bae1f5c8
MysteryCrystal <alanmetwally@gmail.com>
2022-07-22 04:16:40 +0100

    stuff
    inventory start

--- zrok/zrok/Item.cs
+++ zrok/zrok/Item.cs
@@ -8,13 +8,33 @@ namespace zrok
 {
     class Item
     {
+        private ItemType Type;
         private string Name;
         private string Description;
+        private int Health;
 
-        public Item(string name, string description)
+        public Item(string name, string description, int health)
         {
             Name = name;
-            Description = description; 
+            Description = description;
+            Health = health;
+
+            if (health < 0)
+            {
+                Type = ItemType.Weapon;
+            }
+            else
+            {
+                Type = ItemType.Food;
+            }
+        }
+
+        public Item(string name, string description, int health, ItemType type)
+        {
+            Name = name;
+            Description = description;
+            Health = health;
+            Type = type;
         }
     }
 }
